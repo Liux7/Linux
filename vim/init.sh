48dd1059859a27cd2de976265b3c9bacf1f8e008
Liux7 <1042448463@qq.com>
2022-11-02 15:57:58 +0800

    add proxy

--- vim/init.sh
+++ vim/init.sh
@@ -1,1 +1,1 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --proxy "127.0.0.1:7890"
