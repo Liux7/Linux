a5e985cc69e015be86a037593b1811386752fc76
Liux7 <1042448463@qq.com>
2022-11-02 21:15:12 +0800

    .

--- web/webserver.c
+++ web/webserver.c
@@ -161,22 +161,25 @@ void web(int fd, int hit)
 int main(int argc, char **argv)
 {
 
-    int i, port, listenfd, socketfd, hit; socklen_t length;
+    int i, pid, port, listenfd, socketfd, hit; socklen_t length;
     static struct sockaddr_in cli_addr; /* static = initialised to zeros */ static struct sockaddr_in serv_addr; /* static = initialised to zeros */
 /*deal input format begin*/
     if( argc < 3 || argc > 3 || !strcmp(argv[1], "-?") ) {
         (void)printf("hint: nweb Port-Number Top-Directory\t\tversion %d\n\n" "\tnweb is a small and very safe mini web server\n"
         "\tnweb only servers out file/web pages with extensions named below\n" "\t and only from the named directory or its sub-directories.\n"
         "\tThere is no fancy features = safe and secure.\n\n" "\tExample:webserver 8181 /home/nwebdir &\n\n" "\tOnly Supports:", VERSION);
-        for(i=0;extensions[i].ext != 0;i++) (void)printf(" %s",extensions[i].ext);
+        for(i=0;extensions[i].ext != 0;i++) 
+            (void)printf(" %s",extensions[i].ext);
 
         (void)printf("\n\tNot Supported: URLs including \"..\", Java, Javascript, CGI\n" "\tNot Supported: directories / /etc /bin /lib /tmp /usr /dev /sbin \n"
-        "\tNo warranty given or implied\n\tNigel Griffiths nag@uk.ibm.com\n" ); exit(0);
+        "\tNo warranty given or implied\n\tNigel Griffiths nag@uk.ibm.com\n" ); 
+        exit(0);
     }
     if( !strncmp(argv[2],"/"	,2 ) || !strncmp(argv[2],"/etc", 5 ) ||
     !strncmp(argv[2],"/bin",5 ) || !strncmp(argv[2],"/lib", 5 ) ||
     !strncmp(argv[2],"/tmp",5 ) || !strncmp(argv[2],"/usr", 5 ) ||
-    !strncmp(argv[2],"/dev",5 ) || !strncmp(argv[2],"/sbin",6) ){ (void)printf("ERROR: Bad top directory %s, see nweb -?\n",argv[2]); exit(3);
+    !strncmp(argv[2],"/dev",5 ) || !strncmp(argv[2],"/sbin",6) ){ (void)printf("ERROR: Bad top directory %s, see nweb -?\n",argv[2]); 
+    exit(3);
     }
     if(chdir(argv[2]) == -1){
     (void)printf("ERROR: Can't Change to directory %s\n",argv[2]); exit(4);
@@ -196,8 +199,9 @@ int main(int argc, char **argv)
     
     if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0) 
         logger(ERROR, "system call","socket",0);
-    port = atoi(argv[1]); if(port < 0 || port >60000)
-    logger(ERROR,"Invalid port number (try 1->60000)",argv[1],0); 
+    port = atoi(argv[1]); 
+    if(port < 0 || port >60000)
+        logger(ERROR,"Invalid port number (try 1->60000)",argv[1],0); 
     
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -208,16 +212,17 @@ int main(int argc, char **argv)
         
     if( listen(listenfd,64) <0) 
         logger(ERROR,"system call","listen",0);
-	int pid;
+	
 		
     for(hit=1; ;hit++) {
-	  if((pid = fork()) < 0) {
-  	    logger(ERROR,"system call","fork",0);
-  	  }
-	  else
-	  {
-		  if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
+	    if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
 			logger(ERROR,"system call","accept",0);
+        if((pid = fork()) < 0) {
+  	        logger(ERROR,"system call","fork",0);
+  	    }
+	    else
+	    {
+		
 		  if(pid == 0) {   /* child */
     	    (void)close(listenfd);
     	    web(socketfd,hit); /* never returns */
