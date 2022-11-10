053839d4f5f3fce175677f58d5d34b1c55b45635
Liux7 <1042448463@qq.com>
2022-11-10 09:48:17 +0800

    test

--- web/src/webserver.c
+++ web/src/webserver.c
@@ -102,7 +102,27 @@ void logger(int type, char *s1, char *s2, int socket_fd)
     if(type == ERROR || type == NOTFOUND || type == FORBIDDEN) exit(3);
 }
 
+
+void fileprintf(char *text, int id)
+{
+    char timebuff[60];
+	time_t timer;
+	struct tm *Now;
+	time(&timer);
+	Now = localtime(&timer);
+
+	int fp = open("file.log",O_WRONLY | O_APPEND);
+	(void)sprintf(timebuff, "%s id:%d\n", text, id);
+
+	(void)write(fp, timebuff, strlen(timebuff));
+	(void)close(fp);
+}
+
+
+
+
 double socketRead = 0.0, socketWrite = 0.0, logtime = 0.0, readhtml = 0.0;
+pthread_mutex_t gLock;
 
 /* this is a child web server process, so we can exit on errors */ 
 void* web(void* data)
@@ -110,18 +130,33 @@ void* web(void* data)
     struct timeval t0, t1, t2, t3;
     int fd; int hit;
 
+    pthread_mutex_lock(&gLock);
+    
+
     webparam *param=(webparam*) data;
     fd=param->fd;
     hit=param->hit;
     
     double tmptime = 0.0;
-    int j, file_fd, buflen; long i, ret, len; char * fstr;
+    int j, file_fd, buflen; 
+    long i, ret, len; 
+    char * fstr;
     static char buffer[BUFSIZE+1]; /* static so zero filled */
 
     /*===========================read socket=================================*/
+
+    
+    
     gettimeofday(&t0, NULL);
-    ret =read(fd,buffer,BUFSIZE);	/* read Web request in one go */ 
+
+
+
+    ret =read(fd,buffer,BUFSIZE);	/* read Web request in one g    o */ 
+
+
     gettimeofday(&t1, NULL);
+    
+    
     tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
     socketRead += tmptime;
 
@@ -131,102 +166,106 @@ void* web(void* data)
     if(ret == 0 || ret == -1) { /* read failure stop now */
         logger(FORBIDDEN,"failed to read browser request","",fd);
     }
-    if(ret > 0 && ret < BUFSIZE) /* return code is valid chars */ 
-        buffer[ret]=0;	/* terminate the buffer */
-    else 
-        buffer[0]=0;
+	else
+	{
+        if(ret > 0 && ret < BUFSIZE) /* return code is valid chars */ 
+            buffer[ret]=0;	/* terminate the buffer */
+        else 
+          buffer[0]=0;
 
 
         
-    for(i=0;i<ret;i++) /* remove CF and LF characters */ 
-        if(buffer[i] == '\r' || buffer[i] == '\n')
-            buffer[i]='*'; 
+        for(i=0;i<ret;i++) /* remove CF and LF characters */ 
+            if(buffer[i] == '\r' || buffer[i] == '\n')
+                buffer[i]='*'; 
 
-    logger(LOG,"request",buffer,hit);
-    if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) 
-    { 
-        logger(FORBIDDEN,"Only simple GET operation supported",buffer,fd);
-    }
-    for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */ 
-        if(buffer[i] == ' ') 
-        { /* string is "GET URL " +lots of other stuff */
-            buffer[i] = 0; break;
+        logger(LOG,"request",buffer,hit);
+        
+        if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) 
+        { 
+            logger(FORBIDDEN,"Only simple GET operation supported",buffer,fd);
         }
-    }
-    for(j=0;j<i-1;j++)	/* check for illegal parent directory use .. */ 
-    if(buffer[j] == '.' && buffer[j+1] == '.') 
-    {
-        logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
+        for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */ 
+            if(buffer[i] == ' ') 
+            { /* string is "GET URL " +lots of other stuff */
+                buffer[i] = 0; break;
+            }
+        }
+        for(j=0;j<i-1;j++)	/* check for illegal parent directory use .. */ 
+        if(buffer[j] == '.' && buffer[j+1] == '.') 
+        {
+            logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
+    
+        }
+        if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) 
+            (void)strcpy(buffer,"GET /index.html");
  
-    }
-    if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) 
-        (void)strcpy(buffer,"GET /index.html");
- 
-    gettimeofday(&t1, NULL);
-    tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
-    logtime += tmptime;
+        gettimeofday(&t1, NULL);
+        tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
+        logtime += tmptime;
     
 
-    /* work out the file type and check we support it */ 
-    buflen=strlen(buffer);
-    fstr = (char *)0; 
-    for(i=0;extensions[i].ext != 0;i++) 
-    {
-        len = strlen(extensions[i].ext);
-        if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) 
-        { 
-            fstr =extensions[i].filetype;
-            break;
+        /* work out the file type and check we support it */ 
+            buflen=strlen(buffer);
+            fstr = (char *)0; 
+        for(i=0;extensions[i].ext != 0;i++) 
+        {
+            len = strlen(extensions[i].ext);
+            if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) 
+            { 
+                fstr =extensions[i].filetype;
+                break;
+            }
         }
-    }
-    if(fstr == 0) logger(FORBIDDEN,"file extension type not supported",buffer,fd);
+        if(fstr == 0) logger(FORBIDDEN,"file extension type not supported",buffer,fd);
 
-    if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) 
-    { /* open the file for reading */ 
-        logger(NOTFOUND, "failed to open file",&buffer[5],fd);
-    }
+        if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) 
+        { /* open the file for reading */ 
+            logger(NOTFOUND, "failed to open file",&buffer[5],fd);
+        }
 
 /*==============================log=========================================*/
-    gettimeofday(&t0, NULL);
-    logger(LOG,"SEND",&buffer[5],hit);
-    
-    
+        gettimeofday(&t0, NULL);
+        logger(LOG,"SEND",&buffer[5],hit);
+        
+        
 
-    len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */ 
-    (void)lseek(file_fd, (off_t)0, SEEK_SET); /* lseek back to the file start ready for reading */
-    (void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: \
-    close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */ 
+        len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */ 
+        (void)lseek(file_fd, (off_t)0, SEEK_SET); /* lseek back to the file start ready for reading */
+        (void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: \
+        close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */ 
 
-   
-    logger(LOG,"Header",buffer,hit);
-    gettimeofday(&t1, NULL);
-    tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
-    logtime += tmptime;
     
-    /*===========================write socket=================================*/
-    gettimeofday(&t0, NULL);
-    (void)write(fd,buffer,strlen(buffer));
-    gettimeofday(&t1, NULL);
-    tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
-    socketWrite += tmptime;
+        logger(LOG,"Header",buffer,hit);
+        gettimeofday(&t1, NULL);
+        tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
+        logtime += tmptime;
+        
+        /*===========================write socket=================================*/
+        gettimeofday(&t0, NULL);
+        (void)write(fd,buffer,strlen(buffer));
+        gettimeofday(&t1, NULL);
+        
+        tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec); 
+        socketWrite += tmptime;
 
 
 /* send file in 8KB block - last block may be smaller */ 
-    gettimeofday(&t0, NULL);
-    while ((ret = read(file_fd, buffer, BUFSIZE)) > 0) 
-    {
-        (void)write(fd,buffer,ret);
+        gettimeofday(&t0, NULL);
+        while ((ret = read(file_fd, buffer, BUFSIZE)) > 0) 
+        {
+            (void)write(fd,buffer,ret);
+        }
+        gettimeofday(&t1, NULL);
+        tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec);
+        readhtml += tmptime;
+        usleep(10000); /* allow socket to drain before signalling the socket is closed */ 
+    
+    // exit(1);
+        close(file_fd); 
     }
-    gettimeofday(&t1, NULL);
-    tmptime = (t1.tv_sec - t0.tv_sec)*1e6 + (t1.tv_usec - t0.tv_usec);
-    readhtml += tmptime;
-    sleep(1); /* allow socket to drain before signalling the socket is closed */ 
     close(fd);
-   // exit(1);
-   
-   free(param);
-   
-
+    free(param);
 }
 
 
@@ -326,7 +365,10 @@ void* web(void* data)
 
 int main(int argc, char **argv)
 {
-    int i, port, pid, listenfd, socketfd, hit; socklen_t length;
+
+		
+    int i, port, pid, listenfd, socketfd, hit; 
+    socklen_t length;
     static struct sockaddr_in cli_addr; /* static = initialised to zeros */ 
     static struct sockaddr_in serv_addr; /* static = initialised to zeros */
 
@@ -344,8 +386,10 @@ int main(int argc, char **argv)
     if( !strncmp(argv[2],"/"	,2 ) || !strncmp(argv[2],"/etc", 5 ) ||
     !strncmp(argv[2],"/bin",5 ) || !strncmp(argv[2],"/lib", 5 ) ||
     !strncmp(argv[2],"/tmp",5 ) || !strncmp(argv[2],"/usr", 5 ) ||
-    !strncmp(argv[2],"/dev",5 ) || !strncmp(argv[2],"/sbin",6) ){ (void)printf("ERROR: Bad top directory %s, see nweb -?\n",argv[2]); 
-    exit(3);
+    !strncmp(argv[2],"/dev",5 ) || !strncmp(argv[2],"/sbin",6) )
+    { (
+        void)printf("ERROR: Bad top directory %s, see nweb -?\n",argv[2]); 
+        exit(3);
     }
     if(chdir(argv[2]) == -1){
     (void)printf("ERROR: Can't Change to directory %s\n",argv[2]); exit(4);
@@ -354,12 +398,15 @@ int main(int argc, char **argv)
     if(fork() != 0)
         return 0; 
     /* parent returns OK to shell */ 
-    (void)signal(SIGCLD, SIG_IGN); /* ignore child death */ 
-    (void)signal(SIGHUP, SIG_IGN); /* ignore terminal hangups */ 
-    for(i=0;i<32;i++)
-        (void)close(i);		/* close open files */ 
+   (void)signal(SIGCLD,  SIG_IGN); /* ignore child death */ 
+   (void)signal(SIGHUP, SIG_IGN); /* ignore terminal hangups */ 
+   for(i=0;i<32;i++)
+       (void)close(i);		/* close open files */
+
+		printf("hello1\n");
     (void)setpgrp();	/* break away from process group */ 
     
+		printf("hello2\n");
     logger(LOG,"nweb starting",argv[1],getpid());
         /* setup the network socket */
     if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0) 
@@ -381,7 +428,11 @@ int main(int argc, char **argv)
 
     if(bind(listenfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr)) <0) 
         logger(ERROR,"system call","bind",0);
-        
+	int val = 1;
+    if(setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,(void*)&val, sizeof(int))== -1)
+	{
+		exit(1);
+	}
     if( listen(listenfd,64) <0) 
         logger(ERROR,"system call","listen",0);
     
@@ -400,40 +451,9 @@ int main(int argc, char **argv)
         perror("create shared memory object error");
         exit(1);
     }
-    // if((shm_fd1=shm_open(SHM_NAME,O_RDWR| O_CREAT,0666)) < 0){ 
-    //     perror("create shared memory object error");
-    //     exit(1);
-    // }
-    // if((shm_fd2=shm_open(SHM_NAME,O_RDWR| O_CREAT,0666)) < 0){ 
-    //     perror("create shared memory object error");
-    //     exit(1);
-    // }
-    // if((shm_fd3=shm_open(SHM_NAME,O_RDWR| O_CREAT,0666)) < 0){ 
-    //     perror("create shared memory object error");
-    //     exit(1);
-    // }
-    // if((shm_fd4=shm_open(SHM_NAME,O_RDWR| O_CREAT,0666)) < 0){ 
-    //     perror("create shared memory object error");
-    //     exit(1);
-    // }
-    // if((shm_fd5=shm_open(SHM_NAME,O_RDWR| O_CREAT,0666)) < 0){ 
-    //     perror("create shared memory object error");
-    //     exit(1);
-    // }
     /* 配 置 共 享 内 存 段 大 小 */ 
     ftruncate(shm_fd, sizeof(double)*6);
-    // ftruncate(shm_fd1, sizeof(double));
-    // ftruncate(shm_fd2, sizeof(double));
-    // ftruncate(shm_fd3, sizeof(double));
-    // ftruncate(shm_fd4, sizeof(double));
-    // ftruncate(shm_fd5, sizeof(int));
-    //将共享内存对象映射到进程
     tmpargv = mmap(NULL, sizeof(double)*6, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    // totalsR = mmap(NULL, sizeof(double), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd1, 0);
-    // totalsW = mmap(NULL, sizeof(double), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd2, 0);
-    // totallog = mmap(NULL, sizeof(double), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd3, 0);
-    // totalrhtml = mmap(NULL, sizeof(double), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd4, 0);
-    // linkcnt = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd4, 0);
 
     if(tmpargv == MAP_FAILED)
     {
@@ -449,6 +469,7 @@ int main(int argc, char **argv)
     }
     struct timeval t1, t2;
 
+		printf("hello3\n");
     for(hit=1; ;hit++) 
     {
         length = sizeof(cli_addr);
@@ -458,51 +479,57 @@ int main(int argc, char **argv)
         param->hit=hit;
         param->fd=socketfd;
         
+        pthread_mutex_init(&gLock, NULL);
 
         struct timeval t1,t2;
 
 
          gettimeofday(&t1, NULL);
 
-        //if(pthread_create(&pth, NULL, &web, (void*)param)<0)
         if(pthread_create(&pth, &attr, &web, (void*)param)<0)
+        // if(pthread_create(&pth, NULL, &web, (void*)param)<0)
         {
             logger(ERROR,"system call","pthread_create",0);
+			printf("error\n");
         }
-
+        if(hit%2==2)
+        {
+            sleep(1);
+        }
     	   
-            gettimeofday(&t2, NULL);
+		
+        gettimeofday(&t2, NULL);
 
-            double ptime =  (t2.tv_sec - t1.tv_sec)*1000 + (t2.tv_usec - t1.tv_usec)/1000;
-            // double ptime =  (t2.tv_sec - t1.tv_sec)*1000; 
-			sem_wait(psem);
-            *(double *)tmpargv += ptime;
-            (*(double *)(tmpargv+1*sizeof(double))) += (socketRead/1000);
-            (*(double *)(tmpargv+2*sizeof(double))) += (socketWrite/1000);
-            (*(double *)(tmpargv+3*sizeof(double))) += (logtime/1000);
-            (*(double *)(tmpargv+4*sizeof(double))) += (readhtml/1000);
-            (*(double*)(tmpargv+5*sizeof(double)))++;
-            int cnt = (int)(*(double*)(tmpargv+5*sizeof(double)));
-            /*=================================*/
-            char buff[1000];
-            int fp2 = open("/home/liux7/Desktop/Linux/web/Web/time.txt",O_CREAT| O_WRONLY | O_APPEND,0644);
+        double ptime =  (t2.tv_sec - t1.tv_sec)*1000 + (t2.tv_usec - t1.tv_usec)/1000;
+        // double ptime =  (t2.tv_sec - t1.tv_sec)*1000; 
+        sem_wait(psem);
+        *(double *)tmpargv += ptime;
+        (*(double *)(tmpargv+1*sizeof(double))) += (socketRead/1000);
+        (*(double *)(tmpargv+2*sizeof(double))) += (socketWrite/1000);
+        (*(double *)(tmpargv+3*sizeof(double))) += (logtime/1000);
+        (*(double *)(tmpargv+4*sizeof(double))) += (readhtml/1000);
+        (*(double*)(tmpargv+5*sizeof(double)))++;
+        int cnt = (int)(*(double*)(tmpargv+5*sizeof(double)));
+        /*=================================*/
+        char buff[1000];
+        int fp2 = open("/home/liux7/Desktop/Linux/web/Web/time.txt",O_CREAT| O_WRONLY | O_APPEND,0644);
 
-            (void)sprintf(buff, "%d request's totaltime:%lf\n per request use average time:%lf\n per request read socket average time:%lf\n \
+        (void)sprintf(buff, "%d request's totaltime:%lf\n per request use average time:%lf\n per request read socket average time:%lf\n \
 per request write socket average time:%lf\n per request write log average time:%lf\n \
 per request read html average time:%lf\n \n",               
-                cnt,
-                (*(double *)(tmpargv)),
-                (*(double *)(tmpargv))/cnt,
-                (*(double *) (tmpargv+1*sizeof(double)))/cnt,
-                (*(double *) (tmpargv+2*sizeof(double)))/cnt,
-                (*(double *) (tmpargv+3*sizeof(double)))/cnt,
-                (*(double *) (tmpargv+4*sizeof(double)))/cnt);
-            // (void)sprintf(buff, "%lf sR%lf sW%lf lt%lf rh%lf\n",ptime, socketRead/1000, socketWrite/1000, logtime/1000, readhtml/1000);
-            (void)write(fp2, buff, strlen(buff));
-            (void)close(fp2);
-            /*===================================*/
+            cnt,
+        (*(double *)(tmpargv)),
+        (*(double *)(tmpargv))/cnt,
+        (*(double *) (tmpargv+1*sizeof(double)))/cnt,
+        (*(double *) (tmpargv+2*sizeof(double)))/cnt,
+        (*(double *) (tmpargv+3*sizeof(double)))/cnt,
+        (*(double *) (tmpargv+4*sizeof(double)))/cnt);
+        // (void)sprintf(buff, "%lf sR%lf sW%lf lt%lf rh%lf\n",ptime, socketRead/1000, socketWrite/1000, logtime/1000, readhtml/1000);
+        (void)write(fp2, buff, strlen(buff));
+        (void)close(fp2);
+        /*===================================*/
 
-			sem_post(psem);
+        sem_post(psem);
 
 			
         // pthread_join(pth, NULL);
