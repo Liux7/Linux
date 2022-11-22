9b512d1a64cd17ac881a261768bc350f3c35d1ed
Liux7 <1042448463@qq.com>
2022-11-22 22:49:31 +0800

    init.sh

--- vim/init.sh
+++ vim/init.sh
@@ -1,2 +1,3 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --proxy "127.0.0.1:7890"
 cp ./.vimrc ~/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
