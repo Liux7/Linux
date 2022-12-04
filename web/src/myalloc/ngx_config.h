463394809acff6c8c8c9d39eea6a18085a545c22
Liux7 <1042448463@qq.com>
2022-12-04 17:05:59 +0800

    my malloc

--- web/src/myalloc/ngx_config.h
+++ web/src/myalloc/ngx_config.h
@@ -5,6 +5,73 @@
  */
 
 
+
+
+#ifndef _NGX_POSIX_CONFIG_H_INCLUDED_
+#define _NGX_POSIX_CONFIG_H_INCLUDED_
+#include <sys/types.h>
+#include <sys/time.h>
+#if (NGX_HAVE_UNISTD_H)
+#include <unistd.h>
+#endif
+#if (NGX_HAVE_INTTYPES_H)
+#include <inttypes.h>
+#endif
+#include <stdarg.h>
+#include <stddef.h>             /* offsetof() */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <pwd.h>
+#include <grp.h>
+#include <dirent.h>
+#include <glob.h>
+#include <time.h>
+
+#include <sys/ioctl.h>          /* FIONBIO */
+
+#include <sys/uio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <sched.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>        /* TCP_NODELAY */
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/un.h>
+
+
+
+#ifndef IOV_MAX
+#define IOV_MAX   16
+#endif
+
+
+//#include <ngx_auto_config.h>
+
+
+
+
+#define NGX_LISTEN_BACKLOG  511
+
+#define ngx_debug_init()
+
+
+extern char **environ;
+
+
+#endif /* _NGX_POSIX_CONFIG_H_INCLUDED_ */
+
+
 #ifndef _NGX_CONFIG_H_INCLUDED_
 #define _NGX_CONFIG_H_INCLUDED_
 
@@ -39,7 +106,7 @@
 
 
 #else /* POSIX */
-#include <ngx_posix_config.h>
+// #include <ngx_posix_config.h>
 
 #endif
 
